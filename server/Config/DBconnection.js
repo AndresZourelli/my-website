3cfcc409f1b95869ac1a842a958212bcc2d31f7c
Andres <azourelli@gmail.com>
2019-12-16 21:08:47 -0800

    working on server connection

--- server/Config/DBconnection.js
+++ server/Config/DBconnection.js
@@ -1,4 +1,5 @@
 const Pool = require('pg').Pool;
+
 const pool = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
@@ -6,5 +7,27 @@ const pool = new Pool({
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT
 });
+
+pool.query('CREATE TABLE projects(id SERIAL PRIMARY KEY, name VARCHAR, description VARCHAR)', (err, res) => {
+	if (err) {
+		console.log(err);
+	}
+});
+pool.query(
+	'CREATE TABLE website_address(web_id SERIAL PRIMARY KEY, website_link VARCHAR, CONSTRAINT fk_web_id FOREIGN KEY (web_id) REFERENCES projects (id))',
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
+pool.query(
+	'CREATE TABLE github_address(github_id SERIAL PRIMARY KEY, github_link VARCHAR, CONSTRAINT fk_github_id FOREIGN KEY (github_id) REFERENCES projects (id))',
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const db = require('./Config/DBconnection');
 require('dotenv').config();
 
 //Setup
@@ -13,8 +14,6 @@ app.use(require('morgan')('dev'));
 //body parsing middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.static('./images'));
 
 //exposes a directory or a file to a particular URL so it's contents can be publicly accessed
 app.use(express.static(path.join(__dirname, 'public')));
