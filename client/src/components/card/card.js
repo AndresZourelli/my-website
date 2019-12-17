83fecbd3f7af713d3548ea2da782f85c1dd4f0f1
Andres <azourelli@gmail.com>
2019-12-16 17:38:15 -0800

    dynamic buttons

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,18 +1,17 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import './card.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Card = ({ website, title, github, description }) => {
+	let optionalButton = <button href={website}>See it Live</button>;
 	return (
 		<div className="CardBody">
 			<div className="info">
 				<h2>{title}</h2>
 				<p>{description}</p>
 				<div className="button-container">
-					<button href={website}>See it Live</button>
-
+					{website ? optionalButton : null}
 					<button href={github}>
 						View on <FontAwesomeIcon icon={faGithub} size="1x" />
 					</button>
