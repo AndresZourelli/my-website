f8927ecbd1100b8749d9d52d6669223fb85bf376
Andres <azourelli@gmail.com>
2019-12-18 19:26:04 -0800

    added pgadmin container

--- server/Config/DBconnection.js
+++ server/Config/DBconnection.js
@@ -1,37 +1,37 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT
+	user: process.env.POSTGRES_USER,
+	host: localhost,
+	database: process.env.POSTGRES_DB,
+	password: process.env.POSTGRES_PASSWORD,
+	port: 27017
 });
 
-// pool.query(
-// 	'CREATE TABLE projects(id SERIAL PRIMARY KEY, name VARCHAR, description VARCHAR, date TIMESTAMP )',
-// 	(err, res) => {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 	}
-// );
-// pool.query(
-// 	'CREATE TABLE website_address(web_id SERIAL PRIMARY KEY, website_link VARCHAR, CONSTRAINT fk_web_id FOREIGN KEY (web_id) REFERENCES projects (id))',
-// 	(err, res) => {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 	}
-// );
-// pool.query(
-// 	'CREATE TABLE github_address(github_id SERIAL PRIMARY KEY, github_link VARCHAR, CONSTRAINT fk_github_id FOREIGN KEY (github_id) REFERENCES projects (id))',
-// 	(err, res) => {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 	}
-// );
+pool.query(
+	'CREATE TABLE projects(id SERIAL PRIMARY KEY, name VARCHAR, description VARCHAR, date TIMESTAMP )',
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
+pool.query(
+	'CREATE TABLE website_address(web_id SERIAL PRIMARY KEY, website_link VARCHAR, CONSTRAINT fk_web_id FOREIGN KEY (web_id) REFERENCES projects (id))',
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
+pool.query(
+	'CREATE TABLE github_address(github_id SERIAL PRIMARY KEY, github_link VARCHAR, CONSTRAINT fk_github_id FOREIGN KEY (github_id) REFERENCES projects (id))',
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
 
 const addProject = (request, response) => {
 	const { name, description } = request.body;
