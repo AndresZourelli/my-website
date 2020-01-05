76ee49ea84ab222ffdb066449a427b88c69dfc5a
Andres <azourelli@gmail.com>
2020-01-04 17:09:38 -0800

    change script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ docker build -t andreszourelli/web-client:latest -t andreszourelli/web-client:$S
 docker build -t andreszourelli/web-nginx:latest -t andreszourelli/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
 docker build -t andreszourelli/web-server:latest -t andreszourelli/web-server:$SHA -f ./server/Dockerfile ./server
 
-docker push gcr.io/charged-formula-262616/andreszourelli/web-client:$SHA
-docker push gcr.io/charged-formula-262616/andreszourelli/web-nginx:$SHA
-docker push gcr.io/charged-formula-262616/andreszourelli/web-server:$SHA
+docker push gcr.io/charged-formula-262616/web-client
+docker push gcr.io/charged-formula-262616/web-nginx
+docker push gcr.io/charged-formula-262616/web-server
 
 gcloud compute project-info describe --project charged-formula-262616
