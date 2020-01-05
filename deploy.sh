01644ed90df95258ddd87f6012f9f865565f0b89
Andres <azourelli@gmail.com>
2020-01-04 17:30:43 -0800

    aaaagain

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ docker push gcr.io/charged-formula-262616/web-client
 docker push gcr.io/charged-formula-262616/web-nginx
 docker push gcr.io/charged-formula-262616/web-server
 
-gcloud pull gcr.io/charged-formula-262616/web-client
-gcloud pull gcr.io/charged-formula-262616/web-nginx
-gcloud pull gcr.io/charged-formula-262616/web-server
+gcloud docker pull gcr.io/charged-formula-262616/web-client
+gcloud docker pull gcr.io/charged-formula-262616/web-nginx
+gcloud docker pull gcr.io/charged-formula-262616/web-server
 
-gcloud run gcr.io/charged-formula-262616/web-client
-gcloud run gcr.io/charged-formula-262616/web-nginx
-gcloud run gcr.io/charged-formula-262616/web-server
+gcloud docker run gcr.io/charged-formula-262616/web-client
+gcloud docker run gcr.io/charged-formula-262616/web-nginx
+gcloud docker run gcr.io/charged-formula-262616/web-server
