903c036237644e186da3dfa0117f0dbbd38fd9d4
Andres <azourelli@gmail.com>
2020-01-04 17:15:33 -0800

    another

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-docker build -t andreszourelli/web-client:latest -t andreszourelli/web-client:$SHA  -f ./client/Dockerfile ./client
-docker build -t andreszourelli/web-nginx:latest -t andreszourelli/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
-docker build -t andreszourelli/web-server:latest -t andreszourelli/web-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gcr.io/charged-formula-262616/web-client:latest -t gcr.io/charged-formula-262616/web-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t gcr.io/charged-formula-262616/web-nginx:latest -t gcr.io/charged-formula-262616/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
+docker build -t gcr.io/charged-formula-262616/web-server:latest -t gcr.io/charged-formula-262616/web-server:$SHA -f ./server/Dockerfile ./server
 
-docker push gcr.io/charged-formula-262616/andreszourelli/web-client
-docker push gcr.io/charged-formula-262616/andreszourelli/web-nginx
-docker push gcr.io/charged-formula-262616/andreszourelli/web-server
+docker push gcr.io/charged-formula-262616/web-client
+docker push gcr.io/charged-formula-262616/web-nginx
+docker push gcr.io/charged-formula-262616/web-server
 
 gcloud compute project-info describe --project charged-formula-262616
