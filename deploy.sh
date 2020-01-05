da0986a70d9ed2460e1d229239fc93b4b15c96a3
Andres <azourelli@gmail.com>
2020-01-04 20:47:51 -0800

    moved auth

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker push gcr.io/charged-formula-262616/web-client
 docker push gcr.io/charged-formula-262616/web-nginx
 docker push gcr.io/charged-formula-262616/web-server
 
-gcloud auth configure-docker
+
 
 gcloud beta compute --project "charged-formula-262616" ssh --zone "us-west1-b" "instance-1" << EOF
     docker pull gcr.io/charged-formula-262616/web-client
