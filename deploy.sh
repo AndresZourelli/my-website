c9f05f2c352761851af7c2e8a9afa3f8064d4862
Andres <azourelli@gmail.com>
2020-01-04 21:46:58 -0800

    once more

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ docker push gcr.io/charged-formula-262616/web-server
 
 
 gcloud beta compute --project "charged-formula-262616" ssh --zone "us-west1-b" "instance-1" \
-    --command="sudo docker ps; sudo docker pull gcr.io/charged-formula-262616/web-client; sudo docker pull gcr.io/charged-formula-262616/web-nginx; sudo docker pull gcr.io/charged-formula-262616/web-server; sudo docker run -d gcr.io/charged-formula-262616/web-client; sudo docker run -d gcr.io/charged-formula-262616/web-nginx; sudo docker run -d gcr.io/charged-formula-262616/web-server"
+    --command="sudo docker ps; gcloud auth configure-docker; sudo docker pull gcr.io/charged-formula-262616/web-client; sudo docker pull gcr.io/charged-formula-262616/web-nginx; sudo docker pull gcr.io/charged-formula-262616/web-server; sudo docker run -d gcr.io/charged-formula-262616/web-client; sudo docker run -d gcr.io/charged-formula-262616/web-nginx; sudo docker run -d gcr.io/charged-formula-262616/web-server"
