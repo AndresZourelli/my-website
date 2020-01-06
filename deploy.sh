d2defdeebdf05d2ad846ceb70f964166f0a6c70c
Andres <azourelli@gmail.com>
2020-01-05 19:14:00 -0800

    a

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,12 @@ docker push gcr.io/charged-formula-262616/web-server
 #  sudo docker container stop $(docker container ls -aq)
 ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << EOF
 
-gcloud auth list
+ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.1-linux-x86_64.tar.gz
+
+ tar zxvf google-cloud-sdk-274.0.1-linux-x86_64.tar.gz google-cloud-sdk
+
+ ./google-cloud-sdk/install.sh
+ 
  sudo docker pull gcr.io/charged-formula-262616/web-nginx
  
  sudo docker pull gcr.io/charged-formula-262616/web-client
