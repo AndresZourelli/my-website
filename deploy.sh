31bfa46b2cccf16894870accc6e3e222fe12c06b
Andres <azourelli@gmail.com>
2020-01-05 17:01:31 -0800

    removed gclouod commands

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,6 @@ ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << 
 
  sudo docker ps
 
- gcloud auth configure-docker
- 
  sudo docker pull gcr.io/charged-formula-262616/web-client
  sudo docker pull gcr.io/charged-formula-262616/web-nginx
  sudo docker pull gcr.io/charged-formula-262616/web-server
