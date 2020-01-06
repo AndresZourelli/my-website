6dc78f04d237ba0edab0c7438fa0756f859202bf
Andres <azourelli@gmail.com>
2020-01-05 19:39:20 -0800

    a

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,16 @@ docker push gcr.io/charged-formula-262616/web-server
 #  sudo docker container stop $(docker container ls -aq)
 ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << EOF
 
- gcloud auth configure-docker
+ VERSION=1.5.0
+ OS=linux
+ ARCH=amd64
 
+ curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+  | tar xz --to-stdout ./docker-credential-gcr \
+  | sudo tee /usr/bin/docker-credential-gcr > /dev/null && sudo chmod +x /usr/bin/docker-credential-gcr
+
+ 
+ docker-credential-gcr configure-docker
  sudo docker pull gcr.io/charged-formula-262616/web-nginx
  
  sudo docker pull gcr.io/charged-formula-262616/web-client
