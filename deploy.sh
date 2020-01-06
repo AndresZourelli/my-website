4bfeca2ce847b62b31e24b8b2ac9303667f1473d
Andres <azourelli@gmail.com>
2020-01-05 19:59:46 -0800

    b

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ docker push gcr.io/charged-formula-262616/web-server
 ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << EOF
 
  gcloud auth configure-docker
+
  sudo docker pull gcr.io/charged-formula-262616/web-nginx
  
  sudo docker pull gcr.io/charged-formula-262616/web-client
@@ -23,9 +24,13 @@ ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << 
  sudo docker pull gcr.io/charged-formula-262616/web-server
 
  sudo docker run --rm -d -p 3000:3000 gcr.io/charged-formula-262616/web-client
+
  sudo docker run --rm -d -p 80:80 -p 443:443 gcr.io/charged-formula-262616/web-nginx
+
  sudo docker run --rm -d -p 5000:5000 gcr.io/charged-formula-262616/web-server
+
  sudo docker run --rm -d -v /database_data:/var/lib/postgresql/data -e POSTGRES_USER -e POSTGRES_PASSWORD -e POSTGRES_DB postgres 
+
 EOF
 
 
