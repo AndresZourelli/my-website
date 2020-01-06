3aa7fd13371832c1cc005c23a058998bca7988f2
Andres <azourelli@gmail.com>
2020-01-05 19:58:12 -0800

    a

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,7 @@ docker push gcr.io/charged-formula-262616/web-server
 #  sudo docker container stop $(docker container ls -aq)
 ssh -o StrictHostKeyChecking=no -i deploy_key_open travis-ci@104.196.226.118 << EOF
 
- docker-credential-gcr configure-docker
- sudo rm /usr/bin/snap
- sudo rm /usr/bin/docker-credential-gcr
+ gcloud auth configure-docker
  sudo docker pull gcr.io/charged-formula-262616/web-nginx
  
  sudo docker pull gcr.io/charged-formula-262616/web-client
