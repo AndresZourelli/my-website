9b7292ef0dc732a942a4a1de6934a160bb118e48
Andres <azourelli@gmail.com>
2020-01-08 22:34:42 -0800

    paste

--- deploy.sh
+++ deploy.sh
@@ -6,33 +6,27 @@
 # docker push gcr.io/charged-formula-262616/web-nginx
 # docker push gcr.io/charged-formula-262616/web-server
 
-ls
+
 scp ./docker-compose-prod.yml travis@104.196.226.118:~/
-ls
 ssh -i ./deploy_key_open travis@104.196.226.118 << EOF
-
- docker-compose up -f docker-compose-prod.yml
-
-
-
- sudo docker pull gcr.io/charged-formula-262616/web-nginx
- 
- sudo docker pull gcr.io/charged-formula-262616/web-client
- 
- sudo docker pull gcr.io/charged-formula-262616/web-server
-
- sudo docker pull postgres
-
-
-
- sudo docker run --rm -d -p 3000:3000 gcr.io/charged-formula-262616/web-client
-ls
- sudo docker run --rm -d -p 80:80 -p 443:443 gcr.io/charged-formula-262616/web-nginx
-ls
- sudo docker run --rm -d -p 5000:5000 gcr.io/charged-formula-262616/web-server
-ls
- sudo docker run --rm -d -v /database_data:/var/lib/postgresql/data -e POSTGRES_USER -e POSTGRES_PASSWORD -e POSTGRES_DB postgres 
-
+docker-compose up -f docker-compose-prod.yml
 EOF
+#  sudo docker pull gcr.io/charged-formula-262616/web-nginx
+ 
+#  sudo docker pull gcr.io/charged-formula-262616/web-client
+ 
+#  sudo docker pull gcr.io/charged-formula-262616/web-server
+
+#  sudo docker pull postgres
+
+
+
+#  sudo docker run --rm -d -p 3000:3000 gcr.io/charged-formula-262616/web-client
+# ls
+#  sudo docker run --rm -d -p 80:80 -p 443:443 gcr.io/charged-formula-262616/web-nginx
+# ls
+#  sudo docker run --rm -d -p 5000:5000 gcr.io/charged-formula-262616/web-server
+# ls
+#  sudo docker run --rm -d -v /database_data:/var/lib/postgresql/data -e POSTGRES_USER -e POSTGRES_PASSWORD -e POSTGRES_DB postgres 
 
 
