86dc7e12806eba62aaae051614b250f4083b5015
Andres <azourelli@gmail.com>
2020-01-08 21:44:44 -0800

    asdf

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@ openssl aes-256-cbc -K $encrypted_0c35eebf403c_key -iv $encrypted_0c35eebf403c_i
 
 tar xvf secrets.tar
 
+eval $(ssh-agent -s)
+
+chmod 600 deploy_key_open
+
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+ssh-add deploy_key_open
+
 # docker build -t gcr.io/charged-formula-262616/web-client:latest -t gcr.io/charged-formula-262616/web-client:$SHA  -f ./client/Dockerfile ./client
 # docker build -t gcr.io/charged-formula-262616/web-nginx:latest -t gcr.io/charged-formula-262616/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
 # docker build -t gcr.io/charged-formula-262616/web-server:latest -t gcr.io/charged-formula-262616/web-server:$SHA -f ./server/Dockerfile ./server
