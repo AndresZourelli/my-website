7e87d6e825c892c04545adff75a9c84e0b4ab240
Andres <azourelli@gmail.com>
2020-08-28 17:46:16 -0700

    build successfully

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# docker build -t gcr.io/charged-formula-262616/web-client:latest -t gcr.io/charged-formula-262616/web-client:$SHA  -f ./client/Dockerfile ./client
-# docker build -t gcr.io/charged-formula-262616/web-nginx:latest -t gcr.io/charged-formula-262616/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
-# docker build -t gcr.io/charged-formula-262616/web-server:latest -t gcr.io/charged-formula-262616/web-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gcr.io/charged-formula-262616/web-client:latest -t gcr.io/charged-formula-262616/web-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t gcr.io/charged-formula-262616/web-nginx:latest -t gcr.io/charged-formula-262616/web-nginx:$SHA -f ./nginx/Dockerfile ./nginx
+docker build -t gcr.io/charged-formula-262616/web-server:latest -t gcr.io/charged-formula-262616/web-server:$SHA -f ./server/Dockerfile ./server
 
-# docker push gcr.io/charged-formula-262616/web-client
-# docker push gcr.io/charged-formula-262616/web-nginx
-# docker push gcr.io/charged-formula-262616/web-server
+docker push gcr.io/charged-formula-262616/web-client
+docker push gcr.io/charged-formula-262616/web-nginx
+docker push gcr.io/charged-formula-262616/web-server
 
 
 
-# scp ./docker-compose-prod.yml travis@104.196.226.118:~/
+scp -i super_secret.txt ./docker-compose-prod.yml travis@104.196.226.118:~/
 
-ssh -vvv -i super_secret.txt travis@104.196.226.118 << EOF
+ssh -i super_secret.txt travis@104.196.226.118 << EOF
 
 docker-compose -f docker-compose-prod.yml pull
 docker-compose -f docker-compose-prod.yml down
